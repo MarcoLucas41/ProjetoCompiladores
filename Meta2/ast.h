e5f08ca101b5b5fc1d58f2f5e354132f26ef0f8a
SkinnyMarco <105551233+MarcoLucas41@users.noreply.github.com>
2023-11-13 21:03:28 +0000

    Update ast.h

--- Meta2/ast.h
+++ Meta2/ast.h
@@ -1,9 +1,8 @@
 #ifndef _AST_H
 #define _AST_H
-struct node *program;
 enum category { Program,Declaration,FuncDeclaration,FuncDefinition,ParamList,FuncBody,ParamDeclaration,StatList,Or,And,Eq,Ne,Lt,Gt,
                 Le,Ge,Add,Sub,Mul,Div,Mod,Not,Minus,Plus,Store,Comma,Call,BitWiseAnd,BitWiseXor,BitWiseOr,Char,ChrLit,Identifier,Int,
-                Short,Natural,Double,Decimal,Void,Null,If,Else,While,Return,Assign};
+                Short,Natural,Double,Decimal,Void,Null,If,Else,While,Return};
 struct node {
     enum category category;
     char *token;
@@ -16,7 +15,11 @@ struct node_list {
 };
 
 struct node *newnode(enum category category, char *token);
+struct node_list *newlist();
 void addchild(struct node *parent, struct node *child);
+void addchildren(struct node *parent, struct node_list *new_children);
+void addnephews(struct node_list *parent, struct node_list *new_children);
+void addbrother(struct node_list *root_list, struct node *child);
 char* getCategoryName(enum category category);
 void show(struct node *node, int depth); 
 void cleanup(struct node *node);
