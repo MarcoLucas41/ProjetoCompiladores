2e03882fac9df2cb94827005bcdbf53c0a2dfc5a
Marco Lucas <marlucas2003@gmail.com>
2023-11-28 19:22:52 +0000

    Updating build

--- Meta3/build.sh
+++ Meta3/build.sh
@@ -2,6 +2,6 @@
 rm -f uccompiler lex.yy.c y.tab.c y.tab.h
 yacc -d -v -t -g --report=all uccompiler.y
 lex uccompiler.l
-cc -o uccompiler lex.yy.c y.tab.c ast.c -Wall -Wno-unused-function
-./uccompiler -t -l < test
+cc -o uccompiler lex.yy.c y.tab.c ast.c semantics.c -Wall -Wno-unused-function
+./uccompiler -s < test
 #./uccompiler
