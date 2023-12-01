37cd479689e0a907573a92e3be588d53aba79f6e
Marco Lucas <marlucas2003@gmail.com>
2023-12-01 17:26:59 +0000

    Implemented FuncDeclaration checking

--- Meta3/semantics.c
+++ Meta3/semantics.c
@@ -13,7 +13,7 @@ struct table *global;
 struct table_list *list_tables;
 
 
-void check_declaration(struct node *declaration) 
+void check_declaration(struct node *declaration,struct table *scope) 
 {
     //type of declaration is obtained through first son(<typespec> node)
     struct node *typespec = getchild(declaration, 0);
@@ -21,14 +21,73 @@ void check_declaration(struct node *declaration)
 
     //id of declaration is obtained through second son(<identifier> node)
     struct node *id = getchild(declaration,1);
-    if(search_symbol(global, id->token->token) == NULL) 
+    if(search_symbol(scope, id->token->token) == NULL) 
     {
-        insert_symbol(global, id->token->token, type, declaration);
+        insert_symbol(scope, id->token->token, type, declaration);
     } 
     else 
     {
         printf("Line %d, column %d: Symbol %s already defined\n", id->token->line,id->token->column,id->token->token);
         semantic_errors++;
+    }
+}
+
+void check_statement(struct node *statement,struct table *scope)
+{
+    switch(statement->category)
+    {
+        case Store:
+        case Comma:
+        case Add:
+        case Sub:
+        case Mul:
+        case Div:
+        case Mod:
+        case And:
+        case BitWiseAnd:
+        case BitWiseOr:
+        case BitWiseXor:
+        case Eq:
+        case Ne:
+        case Le:
+        case Ge:
+        case Lt:
+        case Gt:
+            check_statement(getchild(statement,0),scope);
+            check_statement(getchild(statement,1),scope);
+            break;
+        case Plus:
+        case Minus:
+        case Not:
+            check_statement(getchild(statement,0),scope);
+            break;
+        case Identifier:
+            break;
+        case Natural: 
+            statement->type = integer_type;
+            break;
+        case ChrLit:
+            statement->type = char_type;
+        case Decimal:
+            statement->type = double_type;
+            break;
+        default: break;
+    }
+}
+
+void check_body(struct node *body,struct table *scope)
+{
+    struct node_list *temp = body->children;
+    while( (temp = temp->next) != NULL )
+    {
+        if(strcmp(getCategoryName(temp->node->category),"Declaration") == 0)
+        {
+            check_declaration(temp->node,scope);
+        }
+        else 
+        {
+            check_statement(temp->node,scope);
+        }
     }
 }
 
@@ -60,31 +119,74 @@ void check_parameters(struct node *param_list,struct table *scope)
 
 void check_function(struct node *function)
 {
-     //type of function is obtained through first son(<typespec> node)
+     // type of function is obtained through first son(<typespec> node)
     struct node *typespec = getchild(function, 0);
     enum type type_function = category_type(typespec->category);
 
-    //id of function is obtained through second son(<Identifier> node)
+    // id of function is obtained through second son(<Identifier> node)
     struct node *id = getchild(function,1);
-    if(search_symbol(global, id->token->token) == NULL) 
+
+    if(function->category == FuncDeclaration)
     {
-        insert_symbol(global, id->token->token, type_function, function);
-        struct table *scope = (struct table *) malloc(sizeof(struct table));
-        scope->next = NULL;
-
-        insert_symbol(scope, "return", type_function, newnode(Return,NULL)); 
-
-        //list of parameters is obtained through third son(<ParamList> node)
-        check_parameters(getchild(function,2),scope);
-
-        //insert table in table list
-        insert_table(list_tables,scope,id->token->token);
-    } 
-    else 
-    {
-        printf("Line %d, column %d: Symbol %s already defined\n", id->token->line,id->token->column,id->token->token);
-        semantic_errors++;
+        if(search_symbol(global, id->token->token) == NULL) //
+        {
+            insert_symbol(global, id->token->token, type_function, function);
+        }
+        else
+        {
+            printf("Line %d, column %d: Symbol %s already defined\n", id->token->line,id->token->column,id->token->token);
+            semantic_errors++;
+        }
     }
+    if(function->category == FuncDefinition)
+    {
+        struct table *result = search_symbol(global, id->token->token); 
+        if(result == NULL) // this function hasn't been declared or defined, so it will be added to global table and will be printed in the end
+        {
+            insert_symbol(global, id->token->token, type_function, function);
+            struct table *scope = (struct table *) malloc(sizeof(struct table));
+            scope->next = NULL;
+            insert_symbol(scope, "return", type_function, newnode(Return,NULL));
+
+            // list of parameters is obtained through third son(<ParamList> node)
+            check_parameters(getchild(function,2),scope);
+
+            // function body is obtained through 4th son(<FuncBody> node)
+            check_body(getchild(function,3),scope);
+
+            // insert table in table list
+            insert_table(list_tables,scope,id->token->token);
+        }
+        if(result != NULL && result->node->category == FuncDeclaration)// this function has been declared before with the same identifier
+        {
+            int num_arguments_definition = count_children_in_node(getchild(function,2));
+            int num_arguments_declaration = count_children_in_node(getchild(result->node,2));
+            if(num_arguments_declaration != num_arguments_definition)
+            {
+                printf("Line %d, column %d: wrong number of arguments to function %s (got %d, required %d)\n", id->token->line,id->token->column,id->token->token,num_arguments_definition,num_arguments_declaration);
+                semantic_errors++;
+            }
+            else
+            {
+                struct table *scope = (struct table *) malloc(sizeof(struct table));
+                scope->next = NULL;
+                insert_symbol(scope, "return", type_function, newnode(Return,NULL));
+
+                // list of parameters is obtained through third son(<ParamList> node)
+                check_parameters(getchild(function,2),scope);
+
+                // function body is obtained through 4th son(<FuncBody> node)
+                check_body(getchild(function,3),scope);
+
+                // insert table in table list
+                insert_table(list_tables,scope,id->token->token);
+            }
+            
+        } 
+
+    }
+    
+    
 
     
 }
@@ -96,13 +198,35 @@ int check_program(struct node *program)
     global = (struct table *) malloc(sizeof(struct table));
     global->next = NULL;
 
+    struct node *temp = newnode(FuncDeclaration,NULL);
+    struct node *temp2 = newnode(ParamList,NULL);
+    struct node *temp3 = newnode(ParamDeclaration,NULL);
+    addchild(temp,newnode(Int,NULL));
+    addchild(temp,newnode(Identifier, create_token("putchar",0,0)));
+
+    addchild(temp3,newnode(Int,NULL));
+    addchild(temp2,temp3);
+    addchild(temp,temp2);
+    insert_symbol(global, "putchar", integer_type, temp);
+
+    struct node *temp4 = newnode(FuncDeclaration,NULL);
+    struct node *temp5 = newnode(ParamList,NULL);
+    struct node *temp6 = newnode(ParamDeclaration,NULL);
+    addchild(temp4,newnode(Int,NULL));
+    addchild(temp4,newnode(Identifier, create_token("getchar",0,0)));
+
+    addchild(temp6,newnode(Void,NULL));
+    addchild(temp5,temp6);
+    addchild(temp4,temp5);
+    insert_symbol(global, "getchar", integer_type, temp4);
+
     list_tables = (struct table_list *) malloc(sizeof(struct table_list));
     list_tables->next = NULL;
 
     struct node_list *child = program->children;
     while((child = child->next) != NULL)
     {
-        if( strcmp(getCategoryName(child->node->category),"Declaration") == 0 ) check_declaration(child->node);
+        if( strcmp(getCategoryName(child->node->category),"Declaration") == 0 ) check_declaration(child->node,global);
         if( strcmp(getCategoryName(child->node->category),"FuncDefinition") == 0 || strcmp(getCategoryName(child->node->category),"FuncDeclaration") == 0) check_function(child->node);
     }
     return semantic_errors;
@@ -112,6 +236,7 @@ int check_program(struct node *program)
 
 void show_function(struct table *symbol)
 {
+    //tab
     printf("%s  %s(", symbol->identifier, type_name(symbol->type));
     struct node *typespec;
     struct node *param_list = getchild(symbol->node,2);
@@ -129,23 +254,35 @@ void show_function(struct table *symbol)
 }
 void show_symbol_tables() 
 {
+    //int num_arguments;
     struct table *symbol;
     struct table_list *list;
-    printf("==== Global Symbol Table ====\n");
+    //struct node *temp;
+    printf("===== Global Symbol Table =====\n");
     for(symbol = global->next; symbol != NULL; symbol = symbol->next)
     {
-        if(strcmp(getCategoryName(symbol->node->category),"Declaration") == 0) printf("%s  %s\n", symbol->identifier, type_name(symbol->type));
-        if(strcmp(getCategoryName(symbol->node->category),"FuncDefinition") == 0 ) show_function(symbol);
+        if(strcmp(getCategoryName(symbol->node->category),"Declaration") == 0) printf("%s   %s\n", symbol->identifier, type_name(symbol->type));
+        if(strcmp(getCategoryName(symbol->node->category),"FuncDefinition") == 0 || strcmp(getCategoryName(symbol->node->category),"FuncDeclaration") == 0) show_function(symbol);
     }
 
     for(list = list_tables->next; list != NULL; list = list->next)
     {
-        printf("\n==== Function %s Symbol Table ====\n",list->function_name);
+        
+        //num_arguments = count_children_in_node(temp);
+        //printf("%d",num_arguments);
+        printf("\n===== Function %s Symbol Table =====\n",list->function_name);
         for(symbol = list->table->next; symbol != NULL; symbol = symbol->next)
         {
-            printf("%s  %s\n", symbol->identifier, type_name(symbol->type));
+            //tab
+            printf("%s  %s", symbol->identifier, type_name(symbol->type));
+            if(strcmp(getCategoryName(symbol->node->category),"ParamDeclaration") == 0)
+            {
+                //tab
+                printf("    param\n");
+            }
+            else printf("\n");
+            
         }
-
 
     }
 }
