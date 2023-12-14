4d5a78683cb1af5b63d338887249d2cec1382f21
Marco Lucas <marlucas2003@gmail.com>
2023-12-14 10:26:59 +0000

    Saving changes on semantics.h

--- Meta3/semantics.h
+++ Meta3/semantics.h
@@ -22,9 +22,10 @@ struct table *insert_symbol(struct table *table, char *identifier, enum type typ
 struct table *search_symbol(struct table *table, char *identifier);
 
 struct table_list *insert_table(struct table_list *table_list,struct table *table,char *function_name);
+struct table_list *search_function(struct table_list *table_list, char *identifier);
 
 void show_symbol_tables();
-void show_annoted_AST(struct node *node, int depth);
+void show_annoted_AST(struct node *node, int depth, struct table *table);
 void cleanup_symbol_tables();
 
 #endif
